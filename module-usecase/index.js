2c25b815b591ddc10800ce1391fd1fb0f1ca8955
k-kimura <kimura_511@me.com>
2019-11-20 07:46:16 +0900

    モジュールの継承について

---
+++ module-usecase/index.js
@@ -0,0 +1,4 @@
+let a = require('./lib/module-a/index');
+a();
+let b = require('./lib/module-b/index');
+b();
---
+++ module-usecase/lib/module-a/index.js
@@ -0,0 +1,5 @@
+console.log('Initialize module-a index.js');
+
+module.exports = () => {
+  console.log('call module-a index.js');
+};
